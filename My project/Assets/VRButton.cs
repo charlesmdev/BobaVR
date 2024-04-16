7d4792164d5575b57d01f0e5c421d44f5c7de498
Charles Manaois <118550889+charlesmdev@users.noreply.github.com>
2024-04-15 23:34:14 -0700

    Added OnTriggerStay and bool _ButtonPressed
    Unsure if this is the correct way of implementing a freaking button for vr???

--- My project/Assets/VRButton.cs
+++ My project/Assets/VRButton.cs
@@ -8,22 +8,34 @@ public class VRButton : MonoBehaviour
 {
     public float deadTime = 1.0f;
     private bool _deadTimeActive = false;
+    private bool _buttonPressed = false;
     public UnityEvent onPressed, onReleased;
     
     public void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Button" && !_deadTimeActive)
         {
+            _buttonPressed = true;
             onPressed?.Invoke();
-            Debug.LogWarning("Button Pressed");
+            Debug.LogWarning("Button Pressed" + _buttonPressed);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Button" && !_deadTimeActive && !_buttonPressed)
+        {
+            _buttonPressed = true;
+            onPressed?.Invoke();
+            Debug.LogWarning("Button Pressed Stay");
         }
     }
     private void OnTriggerExit(Collider other)
     {
+        _buttonPressed = false;
         if (other.tag == "Button" && !_deadTimeActive)
         {
             onReleased?.Invoke();
-            Debug.LogWarning("Button Released");
+            Debug.LogWarning("Button Released" + _buttonPressed);
             StartCoroutine(WaitForDeadTime());
         }
     }
